ddaa5f04db007e1fefeed13a0222f4a6a9ee8250
Jenny Lin <wc.jennylin@gmail.com>
2019-12-09 00:42:06 -0500

    fix the display

--- public/sketch.js
+++ public/sketch.js
@@ -39,7 +39,7 @@ function setup() {
 
     // Listen for texts from partners
     connection.on("text", function(sender, message) {
-        let id = sender.id;
+        let id = sender;
         let txt = message;
         let p;
         try {
