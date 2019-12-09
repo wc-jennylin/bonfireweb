a2b3fbb2130f6409153191301cd4980a9c897fc6
Jenny Lin <wc.jennylin@gmail.com>
2019-12-09 02:48:09 -0500

    list is full layout

--- public/sketch.js
+++ public/sketch.js
@@ -28,6 +28,7 @@ function setup() {
     // List is full
     connection.on("list-is-full", function(){
         createP("Sorry, only 50 people can play...").addClass("center");
+        createElement("br");
         createP("Join sooner next time!").addClass("center");
         connection.close();
     });
