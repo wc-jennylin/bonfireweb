d5dfcdc267f8c701692394cadd2c642d351fc2ac
Jenny Lin <wc.jennylin@gmail.com>
2019-12-09 02:37:11 -0500

    list is full

--- public/sketch.js
+++ public/sketch.js
@@ -27,7 +27,7 @@ function setup() {
     noCanvas();
     // List is full
     connection.on("list-is-full", function(){
-        createP("Sorry, only 50 people can play...").addClass('center');
+        createP("Sorry, only 50 people can play...").addClass("center");
         // createElement("br");
         createP("Join sooner next time!").addClass("center");
         connection.close();
