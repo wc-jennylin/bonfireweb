5d5a3d668eac5250563209c44c1044cf63d43518
Jenny Lin <wc.jennylin@gmail.com>
2019-12-18 16:01:56 -0500

    fixing

--- public/sketch.js
+++ public/sketch.js
@@ -64,8 +64,8 @@ function setup() {
 
         //document.addEventListener('DOMContentLoaded', () => {
             setPlatformInfo();
-            //var inputBox = document.getElementById("inputBox");
-            var inputBox = document.querySelector('.safari #inputBox');
+            var inputBox = document.getElementById("inputBox");
+            // var inputBox = document.querySelector('.safari #inputBox');
             console.log(inputBox);
             if(inputBox) {
               inputBox.addEventListener('focus', function(e) {
