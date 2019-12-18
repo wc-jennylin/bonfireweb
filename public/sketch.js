ec6f68496b5a0d4592b77a3cf78524c884f9df8c
Jenny Lin <wc.jennylin@gmail.com>
2019-12-18 17:31:32 -0500

    fixing

--- public/sketch.js
+++ public/sketch.js
@@ -41,7 +41,7 @@ function setup() {
 
     // Onboarding
     connection.on("you-got-in", function(){
-        congrats = createP("Welcome to Bonfire!" + "<br>" + "<br>" + "Wait for the count down on the big screen to enter your chatroom...");
+        congrats = createP("Welcome to Bonfire!" + "<br>" + "<br>" + "Wait for the countdown on the big screen to enter your chatroom...");
         congrats.addClass("center");
         // congrats.addClass("fade-out");
     });
